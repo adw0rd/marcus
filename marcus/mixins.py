c89f5afbfa7ade5789855feed54369d323b51cdc
Mikhail Andreev (aka adw0rd) <x11org@gmail.com>
2012-08-14 16:07:34 +0400

    Improved ArticleTextSizeAdminMixin

--- marcus/mixins.py
+++ marcus/mixins.py
@@ -3,11 +3,15 @@ from marcus import models
 
 
 class ArticleTextSizeAdminMixin(object):
-    max_text_size = models.Article.objects\
+    max_text_sizes = models.Article.objects\
         .extra(select={'text_length': 'if(char_length(text_ru)>char_length(text_en),char_length(text_ru),char_length(text_en))'})\
-        .order_by('-text_length')[0].text_length
+        .order_by('-text_length')\
+        .values('text_length')[0:5]
 
     def text_size(self, article):
-        percent = int((len(article.html()) * 100) / self.max_text_size)
-        return '<div style="width:{percent}%;height:15px;background-color:#5B80B2"></div>'.format(percent=int(percent) or 1)
+        max_text_sizes = [i['text_length'] for i in self.max_text_sizes]
+        max_text_size = int(sum(max_text_sizes) / len(max_text_sizes))
+        percent = int((len(article.html()) * 100) / max_text_size)
+        return '<div style="width:100px;overflow:hidden"><div style="width:{percent}%;height:15px;background-color:#5B80B2">'\
+               '</div></div>'.format(percent=int(percent) or 1)
     text_size.allow_tags = True
