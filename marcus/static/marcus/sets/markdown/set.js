26489ee713ce3057f9f6285e6f35b0700f754fd8
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2013-02-18 09:15:54 +0400

    MarkitUp refactoring

--- marcus/static/marcus/sets/markdown/set.js
+++ marcus/static/marcus/sets/markdown/set.js
@@ -26,16 +26,30 @@ var fullScreen = {
 }
 
 mySettings = {
-    previewParserPath:    '/markitup/preview/',
-    onShiftEnter:        {keepDefault:false, openWith:'\n\n'},
+    previewParserPath: '/markitup/preview/',
+    onShiftEnter: {keepDefault: false, openWith: '\n\n'},
     markupSet: [
-        {name:'First Level Heading', key:'1', placeHolder:'Your title here...', closeWith:function(markItUp) { return miu.markdownTitle(markItUp, '=') } },
-        {name:'Second Level Heading', key:'2', placeHolder:'Your title here...', closeWith:function(markItUp) { return miu.markdownTitle(markItUp, '-') } },
+        {
+            name: 'First Level Heading',
+            key: '1',
+            placeHolder: 'Your title here...',
+            closeWith: function(markItUp) {
+                return miu.markdownTitle(markItUp, '=');
+            }
+        },
+        {
+            name: 'Second Level Heading',
+            key: '2',
+            placeHolder: 'Your title here...',
+            closeWith: function(markItUp) {
+                return miu.markdownTitle(markItUp, '-');
+            }
+        },
         {name:'Heading 3', key:'3', openWith:'### ', placeHolder:'Your title here...' },
         {name:'Heading 4', key:'4', openWith:'#### ', placeHolder:'Your title here...' },
         {name:'Heading 5', key:'5', openWith:'##### ', placeHolder:'Your title here...' },
         {name:'Heading 6', key:'6', openWith:'###### ', placeHolder:'Your title here...' },
-        {separator:'---------------' },        
+        {separator:'---------------' },
         {name:'Bold', key:'B', openWith:'**', closeWith:'**'},
         {name:'Italic', key:'I', openWith:'_', closeWith:'_'},
         {separator:'---------------' },
@@ -46,7 +60,7 @@ mySettings = {
         {separator:'---------------' },
         {name:'Picture', key:'P', replaceWith:'![[![Alternative text]!]]([![Url:!:http://]!] "[![Title]!]")'},
         {name:'Link', key:'L', openWith:'[', closeWith:']([![Url:!:http://]!] "[![Title]!]")', placeHolder:'Your text to link here...' },
-        {separator:'---------------'},    
+        {separator:'---------------'},
         {name:'Quotes', openWith:'> '},
         {name:'Code Block / Code', openWith:'(!(\t|!|`)!)', closeWith:'(!(`)!)'},
         {separator:'---------------'},
